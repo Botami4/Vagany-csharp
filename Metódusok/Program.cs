1518d925e590df94ab3cb5f1a33a30c67f4a94f2
Botami4 <milan.botami@gmail.com>
2024-11-05 09:02:53 +0100

    kezdeti metódusok

--- Metódusok/Program.cs
+++ Metódusok/Program.cs
@@ -4,7 +4,23 @@
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello, World!");
+			Koszonto();
+			Console.WriteLine(Osszeadas(4, 5));
+			Console.ReadLine();
+		}
+
+		static void Koszonto()
+		{
+			Console.WriteLine("Hello világ");
+		}
+
+		static int Osszeadas(int a, int b )
+		{
+			/*
+			int osszeg = a + b;
+			return osszeg;
+			*/
+			return a + b;
 		}
 	}
 }
